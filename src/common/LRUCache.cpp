846280d40acff30eb45d34b4a317f4e849c2f775
houliang <neuyilan@163.com>
2018-03-20 21:34:16 +0800

    add new method for the lrucache. 1. it can remove number of N blocks from the cache. 2. it can adjust the max capacity of the lru cache

--- src/common/LRUCache.cpp
+++ src/common/LRUCache.cpp
@@ -25,6 +25,7 @@
 #include <unordered_map>
 #include <list>
 #include <vector>
+#include <assert.h>
 #include "Logger.h"
 
 namespace Gopherwood {
@@ -38,6 +39,11 @@ public:
 
     LRUCache(size_t max_size) :
             _max_size(max_size) {
+    }
+
+    void adjustSize(size_t newSize) {
+        assert(newSize > 0);
+        this->_max_size = newSize;
     }
 
     std::vector<key_t> put(const key_t &key, const value_t &value) {
@@ -59,6 +65,20 @@ public:
             deleteVector.push_back(last->first);
         }
         return deleteVector;
+    }
+
+    std::vector<key_t> removeNumOfKeys(size_t num) {
+        std::vector<key_t> removeVector;
+        size_t removeNum = num > _cache_items_map.size() ? _cache_items_map.size() : num;
+        while (removeNum > 0) {
+            auto last = _cache_items_list.end();
+            last--;
+            _cache_items_map.erase(last->first);
+            _cache_items_list.pop_back();
+            removeVector.push_back(last->first);
+            removeNum--;
+        }
+        return removeVector;
     }
 
     void deleteObject(const key_t &key) {
