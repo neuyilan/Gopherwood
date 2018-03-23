965589f402306dfed50b287b4f917a4caecd000f
houliang <neuyilan@163.com>
2018-03-23 16:57:15 +0800

    add the test file

--- test/function/TestGWAPI.cpp
+++ test/function/TestGWAPI.cpp
@@ -44,7 +44,7 @@ void testGWWrite(std::string fileName) {
 
     /*3. construct the  input source file name*/
     std::stringstream ss;
-    ss << "/ssdfile/ssdkv/" << fileName;
+    ss << "/ssdfile/goworkspace/" << fileName;
     std::string filePath = ss.str();
 
     /*4. read data from file*/
@@ -91,7 +91,7 @@ void testGWRead(string fileName) {
 
     //3. construct the file name
     std::stringstream ss;
-    ss << "/ssdfile/ssdkv/" << fileName << "-readCache";
+    ss << "/ssdfile/goworkspace/" << fileName << "-readCache";
     std::string fileNameForWrite = ss.str();
 
 
